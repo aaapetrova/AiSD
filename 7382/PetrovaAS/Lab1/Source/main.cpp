7efacb5e88d5e43759a8d54aebb58fb2f6868ff3
Anna <anna.petrova.16.11@gmail.com>
2018-09-27 19:17:57 +0300

    changes

--- 7382/PetrovaAS/Lab1/Source/main.cpp
+++ 7382/PetrovaAS/Lab1/Source/main.cpp
@@ -2,46 +2,46 @@
 
 /*Вариант 1*/ 
 
-using namespace std;
 typedef unsigned long long ull;
 
 ull getValue();
-ull rec(ull n, ull m);
+ull rec(ull n, ull m, int k);
 
 int main()
 {
 	/*reading data from the input stream*/
-    cout << "Enter the number for which you want to calculate the binomial coefficient: ";
+    std:: cout << "Enter the number for which you want to calculate the binomial coefficient: ";
     ull n = getValue();
-    cout << n << endl;
+    std:: cout << n << "\n";
 
-    cout << "Enter the number of the coefficient: " ;
+    std:: cout << "Enter the number of the coefficient: " ;
     ull m = getValue();
-    cout << m << endl;
+    std:: cout << m << "\n";
 
 	/*checking the entered data for correctness*/
     if(m > n){
-        cerr << "Invalid input. M can't be larger than N. Aborting!\n";
+        std::cerr << "Invalid input. M can't be larger than N. Aborting!\n";
         exit(1);
     }
-    ull r=rec(n,m);
-    cout << "Answer: [" << r << "]\n";
+    std:: cout << "Intermediate value\n";
+    ull r=rec(n,m,0);
+    std:: cout << "Answer: [" << r << "]\n";
 
     return 0;
 }
 
 ull getValue() {
     ull value;
-    cin >> value;
-    if(!cin.good()) { 
+    std:: cin >> value;
+    if(!std::cin.good()) { 
 		/* If cin have an exception like ERANGE value or symbols in cin it returns false*/
-        cerr << "Error with input. Aborting!";
+        std::cerr << "Error with input. Aborting!";
         exit(1);
     }
     return value;
 }
 
-ull rec(ull n, ull m){
+ull rec(ull n, ull m, int k){
 
 	if (m>n/2)
         m=n-m;
@@ -52,7 +52,13 @@ ull rec(ull n, ull m){
         return 1;
 	/*algorithm for calculating binomial coefficients*/
     ull r;
-	r=rec(n-1,m);
-    r=r+rec(n-1,m-1);
+	r=rec(n-1,m,k+1)+rec(n-1,m-1,k+1);
+    for(int i=0;i<k;i++){
+        std:: cout << " "; 
+    }
+    std:: cout<< r << "\n";
+    for(int i=k;i>0;i--){
+        std:: cout << " "; 
+    }
     return r;
 }
